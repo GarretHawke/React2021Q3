f1efbf6899ae28c3075962e8b544dacba311b198
GarretHawke <alexandra17au@gmail.com>
2021-08-19 21:22:35 +0300

    feat: add about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,21 @@
 import React, { Fragment, useState } from 'react';
-import SearchBar from '@/components/SearchBar';
-import ResultsList from '@/components/ResultsList';
-import { Article, SortType } from '@/shared';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from '@/pages/Home';
+import AboutPage from '@/pages/About';
 
 const App = (): JSX.Element => {
-  const [searchValue, setSearchValue] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [totalResults, setTotalResults] = useState(1);
-  const [articles, setArticles] = useState<Article[] | []>([]);
-  const [sortBy, setSortBy] = useState<SortType>(SortType.relevancy);
-  const [page, setPage] = useState(1);
-  const [resultsPerPage, setResultsPerPage] = useState(10);
-
-  const sortHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    switch (event.target.value) {
-      case SortType.relevancy:
-        setSortBy(SortType.relevancy);
-        break;
-      case SortType.popularity:
-        setSortBy(SortType.popularity);
-        break;
-      case SortType.publishedAt:
-        setSortBy(SortType.publishedAt);
-        break;
-      default:
-        break;
-    }
-  };
   
   return (
-    <Fragment>
-      <SearchBar
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-        isLoading={isLoading}
-        setIsLoading={setIsLoading}
-        setArticles={setArticles}
-        sortBy={sortBy}
-        page={page}
-        resultsPerPage={resultsPerPage}
-        setTotalResults={setTotalResults}
-      />
-      {articles.length !== 0 && (
-        <ResultsList
-          articles={articles}
-          pageNumber={page}
-          setPageNumber={setPage}
-          sortHandler={sortHandler}
-          resultsPerPage={resultsPerPage}
-          setResultsPerPage={setResultsPerPage}
-          totalResults={totalResults}
-        />
-      )}
-    </Fragment>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+        <Route path='/about'>
+          <AboutPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/pages/About/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.scss';
+
+const AboutPage = (): JSX.Element => {
+  return (
+    <div className="about-page">
+      <h2 className="about-header">about</h2>
+    </div>
+  );
+}
+
+export default AboutPage;
