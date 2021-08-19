b91503b3c0a66d8f51eede3407d50660a1e3dc02
GarretHawke <alexandra17au@gmail.com>
2021-08-19 21:46:26 +0300

    feat: add Header nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '@/components/Header';
 import HomePage from '@/pages/Home';
 import AboutPage from '@/pages/About';
 
@@ -7,6 +8,7 @@ const App = (): JSX.Element => {
   
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path='/'>
           <HomePage />
