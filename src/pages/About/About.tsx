378b18b34bf359fcd087d3c675ecb58ffde1180d
GarretHawke <alexandra17au@gmail.com>
2021-08-21 17:07:19 +0300

    feat: add styles to about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -3,8 +3,8 @@ import './About.scss';
 
 const AboutPage = (): JSX.Element => {
   return (
-    <div className="about-page">
-      <h2 className="about-header">about</h2>
+    <div className="about-wrapper">
+      <h1 className="about-header">about</h1>
     </div>
   );
 }
